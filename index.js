3afc77359e5cf01ca2984586762a5428683c792d
Madhur Verma <vmadhur345@gmail.com>
2024-11-27 15:26:50 +0530

    second lastccc

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:['http://localhost:5173','https://jobportal-client-tnc5.onrender.com'],
     credentials:true
 }
 
